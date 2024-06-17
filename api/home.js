459a8fddc6cbf8a42334e2375de6daee80a2d5dc
Gavin Hawwari <122170888+gavinhawaari@users.noreply.github.com>
2024-06-17 22:12:44 +0700

    Update home.js

--- api/home.js
+++ api/home.js
@@ -15,7 +15,7 @@ module.exports = async (req, res) => {
         return;
     }
 
-    const urls = 'https://www.njav.com/id/';
+    const urls = `${targetUrl}id/`;
     
     https.get(urls, (response) => {
         let data = '';
